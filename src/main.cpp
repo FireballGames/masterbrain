7f7c2808fae0e9e44b1e8c9c92f18429f0e53d26
Dmitry Kutsenko <d2emonium@gmail.com>
2016-12-04 19:20:25 +0300

    Main window

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,100 @@ sf::Texture loadBackground()
     return tBackground;
 }
 
+int gameWindow(sf::RenderWindow &window)
+{
+    // Load a sprite to display
+    sf::Texture tBackground = loadBackground();
+    sf::Sprite sBackground(tBackground);
+
+    //  int n = 0;
+    //  for(int i=1; i<blockCols; i++)
+    //      for(int j=1; j<blockRows; j++)
+    //      {
+    //          sBlock[n].setTexture(tBlock);
+    //          sBlock[n].setPosition(i * blockW, j * blockH);
+    //          n++;
+    //      }
+
+    //  sf::Texture tBall;
+    //  if (!tBall.loadFromFile(ball))
+    //      return EXIT_FAILURE;
+    //  sf::Sprite sBall(tBall);
+    //  sBall.setPosition(ballPos[0], ballPos[1]);
+
+    //  sf::Texture tPaddle;
+    //  if (!tPaddle.loadFromFile(paddle))
+    //       return EXIT_FAILURE;
+    //  sf::Sprite sPaddle(tPaddle);
+    //  sPaddle.setPosition(paddlePos[0], paddlePos[1]);
+
+    // float dx = ballSpeed[0];
+    // float dy = ballSpeed[1];
+
+	// Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed)
+                return EXIT_SUCCESS;
+        }
+
+        //  sBall.move(dx, 0);
+        //  for(int i=0; i<n; i++)
+        //      if(isCollide(sBall, sBlock[i]))
+        //      {
+        //          sBlock[i].setPosition(posNowhere[0], posNowhere[1]);
+        //          dx = -dx;
+        //      }
+
+        //  sBall.move(0, dy);
+        //  for(int i=0; i<n; i++)
+        //      if(isCollide(sBall, sBlock[i]))
+        //      {
+        //          sBlock[i].setPosition(posNowhere[0], posNowhere[1]);
+        //          dy = -dy;
+        //      }
+
+        //  sf::Vector2f b = sBall.getPosition();
+        //  if(b.x < 0 || b.x > N)
+        //      dx = -dx;
+        //  if(b.y < 0 || b.y > M)
+        //      dy = -dy;
+
+        //  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        //      sPaddle.move( 6, 0);
+        //  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        //      sPaddle.move(-6, 0);
+
+        //  if(isCollide(sPaddle, sBall))
+        //      dy = -(rand() % 5 + 2);
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sBackground);
+
+        //  for(int i=0; i<n; i++)
+        //  {
+        //      app.draw(sBlock[i]);
+        //  }
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
+
 int mainMenu(sf::RenderWindow &window)
 {
-
     sf::Texture tLogoTitle;
     if (!tLogoTitle.loadFromFile(logoTitle))
         return EXIT_FAILURE;
@@ -102,9 +193,9 @@ int mainMenu(sf::RenderWindow &window)
             if (event.type == sf::Event::KeyPressed)
             {
                 if(event.key.code == sf::Keyboard::Num1)
-                    return EXIT_SUCCESS;
+                    gameWindow(window);
                 if(event.key.code == sf::Keyboard::Num2)
-                    return EXIT_SUCCESS;
+                    gameWindow(window);
                 if(event.key.code == sf::Keyboard::Num3)
                     level = (level < 3) ? level + 1 : 1;
                 if(event.key.code == sf::Keyboard::Num4)
@@ -128,22 +219,20 @@ int mainMenu(sf::RenderWindow &window)
         soundText.setString(sound ? "ON" : "OFF");
         cardMoveText.setString(cardMove ? "YES" : "NO");
 
-        // Clear screen
         window.clear();
 
-        // Draw the sprite
         window.draw(sLogoTitle);
         window.draw(sLogoCredits);
         for(int i=0; i < 7; i++)
         {
             window.draw(menuPoints[i]);
         }
+
         window.draw(levelText);
         window.draw(cardSetText);
         window.draw(soundText);
         window.draw(cardMoveText);
 
-        // Update the window
         window.display();
     }
 
@@ -157,95 +246,9 @@ int main()
     // Create the main window
     sf::RenderWindow app(sf::VideoMode(winSize[0], winSize[1]), gameTitle);
 
-    // Load a sprite to display
-    sf::Texture tBackground = loadBackground();
-    sf::Sprite sBackground(tBackground);
-
     int res = mainMenu(app);
     if(res != EXIT_SUCCESS)
         return res;
-
-    //  int n = 0;
-    //  for(int i=1; i<blockCols; i++)
-    //      for(int j=1; j<blockRows; j++)
-    //      {
-    //          sBlock[n].setTexture(tBlock);
-    //          sBlock[n].setPosition(i * blockW, j * blockH);
-    //          n++;
-    //      }
-
-    //  sf::Texture tBall;
-    //  if (!tBall.loadFromFile(ball))
-    //      return EXIT_FAILURE;
-    //  sf::Sprite sBall(tBall);
-    //  sBall.setPosition(ballPos[0], ballPos[1]);
-
-    //  sf::Texture tPaddle;
-    //  if (!tPaddle.loadFromFile(paddle))
-    //       return EXIT_FAILURE;
-    //  sf::Sprite sPaddle(tPaddle);
-    //  sPaddle.setPosition(paddlePos[0], paddlePos[1]);
-
-    // float dx = ballSpeed[0];
-    // float dy = ballSpeed[1];
-
-	// Start the game loop
-    while (app.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (app.pollEvent(event))
-        {
-            // Close window : exit
-            if (event.type == sf::Event::Closed ||
-                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-                app.close();
-        }
-
-        //  sBall.move(dx, 0);
-        //  for(int i=0; i<n; i++)
-        //      if(isCollide(sBall, sBlock[i]))
-        //      {
-        //          sBlock[i].setPosition(posNowhere[0], posNowhere[1]);
-        //          dx = -dx;
-        //      }
-
-        //  sBall.move(0, dy);
-        //  for(int i=0; i<n; i++)
-        //      if(isCollide(sBall, sBlock[i]))
-        //      {
-        //          sBlock[i].setPosition(posNowhere[0], posNowhere[1]);
-        //          dy = -dy;
-        //      }
-
-        //  sf::Vector2f b = sBall.getPosition();
-        //  if(b.x < 0 || b.x > N)
-        //      dx = -dx;
-        //  if(b.y < 0 || b.y > M)
-        //      dy = -dy;
-
-        //  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        //      sPaddle.move( 6, 0);
-        //  if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        //      sPaddle.move(-6, 0);
-
-        //  if(isCollide(sPaddle, sBall))
-        //      dy = -(rand() % 5 + 2);
-
-        // Clear screen
-        app.clear();
-
-        // Draw the sprite
-        app.draw(sBackground);
-
-        //  for(int i=0; i<n; i++)
-        //  {
-        //      app.draw(sBlock[i]);
-        //  }
-
-        // Update the window
-        app.display();
-    }
 
     return EXIT_SUCCESS;
 }
