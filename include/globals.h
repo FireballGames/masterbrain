519e322dba545423d3967f5f801375fa30454620
Dmitry Kutsenko <d2emonium@gmail.com>
2016-12-31 19:30:55 +0300

    Global vars

---
+++ include/globals.h
@@ -0,0 +1,33 @@
+#ifndef GLOBALS_H_INCLUDED
+#define GLOBALS_H_INCLUDED
+
+#include <SFML/Graphics.hpp>
+
+// JetPac
+// ??? Knife
+// Batman
+// Jack Nipper
+
+// Sabrewolf
+// ----
+// ----
+// Cookie
+
+// Pssst
+// ----
+// ----
+// ??? Head
+
+// ----
+// ??? Viking
+// ??? Panama
+// ----
+
+extern int level;
+extern int cardSetId;
+extern bool sound;
+extern bool cardMove;
+
+extern sf::String cardSets[4];
+
+#endif // GLOBALS_H_INCLUDED
---
+++ src/globals.cpp
@@ -0,0 +1,13 @@
+#include "globals.h"
+
+int level = 2; // 20(4), 30(6), 40(8)
+int cardSetId = 1;
+bool sound = true;
+bool cardMove = false;
+sf::String cardSets[4] = {
+    "NUMBERS",
+    "SPECTRUM HEROES",
+    "KIDS",
+    "COLOUR MASTER",
+};
+
